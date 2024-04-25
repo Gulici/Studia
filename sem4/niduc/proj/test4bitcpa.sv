d864bee6a922f53cfccf4f70af93eff3b98f496a
Krzysztof Czerniachowicz <k.czerniachowicz@gmail.com>
2024-04-25 23:55:16 +0200

    AIZO: prawie koniec

--- sem4/niduc/proj/test4bitcpa.sv
+++ sem4/niduc/proj/test4bitcpa.sv
@@ -10,12 +10,12 @@ module test4bitcpa;
 	CPA_4bit cpa(A,B,cin,err_tab,sum,cout);
 	
 	initial begin 
-	err_tab[0] = 1;
+	err_tab[0] = 0;
 	err_tab[1] = 0;
 	err_tab[2] = 0;
 	err_tab[3] = 0;
 	
-	A = 5; B = 3; cin = 0;
+	A = 16; B =15; cin = 0;
 	#10; 
 	$display("A=%d B=%d S=%d cout=%b", A,B,sum,cout);
 	end
